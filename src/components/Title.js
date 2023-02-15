d368b760cafd2f34bafc4401659862564ab278f1
Jakub Łakomy <kubbaz122@gmail.com>
2023-02-16 00:55:15 +0100

    basic changes, site shaping nad text

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+	return (
+		<div className="title">
+			<h1>Galeria zdjęć</h1>
+			<h2>Twoje zdjęcia</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+		</div>
+	)
+}
+
+export default Title;
